696c59d7a889457951d14b4f65feef13f27d10af
Jonathan Christian <christianj707@gmail.com>
2024-11-28 15:23:46 -0800

    remove unnecessary methods

--- lib/build.rb
+++ lib/build.rb
@@ -1,35 +1,10 @@
 require 'fileutils'
 require 'htmlbeautifier'
 require 'kramdown'
-require 'liquid'
 require 'yaml'
 
 config_file = YAML.load_file("config.yml")
 SITE_NAME = config_file["site-name"]
-
-def read_front_matter(file_path)
-  content = File.read(file_path)
-  if content =~ /\A---\n(.*?)\n---\n/m
-    front_matter = YAML.load($1)
-    body = $'
-    [front_matter, body]
-  else
-    [nil, content]
-  end
-end
-
-def process_markdown(content)
-  Kramdown::Document.new(content, {auto_ids: false}).to_html
-end
-
-def render_template(template_file, assigns)
-  template = File.read(template_file)
-  Liquid::Template.parse(template).render(assigns)
-end
-
-def copy_assets
-  FileUtils.cp_r('assets', 'site')
-end
 
 def build_index_page
   posts = []
@@ -74,21 +49,8 @@ def build_index_page
 end
 
 def build_pages(content_dir)
-  Dir.glob("#{content_dir}/*.md").each do |file|
-    front_matter, body = read_front_matter(file)
-    html_content = process_markdown(body)
+  Dir.glob("#{content_dir}/*.html").each do |file|
 
-    next if content_dir == "content/posts" && front_matter['draft']
-
-    assigns = {
-      'title' => front_matter['title'],
-      'date' => front_matter['date'],
-      'content' => html_content
-    }
-
-    rendered_content = render_template("templates/main_layout.liquid", assigns)
-    output_file = "site/#{front_matter['slug']}.html"
-    File.write(output_file, rendered_content)
   end
 end
 
@@ -98,11 +60,6 @@ def clean_html_files
   end
 end
 
-def remove_html_extensions
-  Dir.glob("site/*html").each do |file|
-    File.rename(file, file.delete_suffix(".html"))
-  end
-end
 
 def build_site
   start_time = Time.now
