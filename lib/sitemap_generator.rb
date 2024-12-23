62aaa174dfe6afbd0b0958e605c2dfc6ae97af84
Jonathan Christian <christianj707@gmail.com>
2024-12-23 15:09:12 -0800

    get module to generate sitemap correctly

--- lib/sitemap_generator.rb
+++ lib/sitemap_generator.rb
@@ -1,17 +1,34 @@
 module SiteMapGenerator
   require 'builder'
+  require 'fileutils'
+  require 'yaml'
 
-  def self.generate(pages, output_path)
+  SITE_DIRECTORY = File.expand_path('../site', __dir__)
+  BASE_URL = YAML.load_file("config.yaml")["site_url"]
+
+  def self.generate
     xml = Builder::XmlMarkup.new(indent: 2)
     xml.instruct! :xml, version: "1.0", encoding: "UTF-8"
     xml.urlset xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9" do
-      pages.each do |page|
+      Dir.glob("#{SITE_DIRECTORY}/**/*").each do |file|
+        next if File.directory?(file)
+        next if file.include?('/assets/')
+
+        relative_path = file.sub("#{SITE_DIRECTORY}/", '')
+        
+        if relative_path == "index.html"
+          url = BASE_URL
+        else
+          url = File.join(BASE_URL, relative_path)
+        end
+
         xml.url do
-          xml.loc page[:url]
-          xml.lastmod page[:lastmod]
+          xml.loc url
+          xml.lastmod File.mtime(file).iso8601
         end
       end
     end
-    File.write(output_path, xml.target!)
+
+    File.write("site/sitemap.xml", xml.target!)
   end
 end
