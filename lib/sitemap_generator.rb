b1a2747279f6ee748dc7ff1dc8fdf2f89df1f31c
Jonathan Christian <christianj707@gmail.com>
2024-12-23 15:44:23 -0800

    add hashing to only update lastmod if file actually changes

--- lib/sitemap_generator.rb
+++ lib/sitemap_generator.rb
@@ -1,12 +1,22 @@
 module SiteMapGenerator
   require 'builder'
+  require 'digest'
   require 'fileutils'
   require 'yaml'
 
   SITE_DIRECTORY = File.expand_path('../site', __dir__)
   BASE_URL = YAML.load_file("config/config.yaml")["site_url"]
+  HASH_FILE = File.expand_path('../file_hashes.yaml', __dir__)
 
   def self.generate
+    if File.exist?(HASH_FILE)
+      existing_hashes = YAML.load_file(HASH_FILE)
+    else
+      existing_hashes = {}
+    end
+
+    new_hashes = {}
+
     xml = Builder::XmlMarkup.new(indent: 2)
     xml.instruct! :xml, version: "1.0", encoding: "UTF-8"
     xml.urlset xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9" do
@@ -22,13 +32,27 @@ module SiteMapGenerator
           url = File.join(BASE_URL, relative_path)
         end
 
+        content = File.read(file)
+        file_hash = Digest::SHA256.hexdigest(content)
+        new_hashes[relative_path] = file_hash
+
+        if existing_hashes[relative_path] == file_hash
+          lastmod = existing_hashes.fetch("#{relative_path}_lastmod", File.mtime(file).iso8601)
+        else
+          lastmod = Time.now.iso8601
+        end
+
+        new_hashes["#{relative_path}_lastmod"] = lastmod
+
         xml.url do
           xml.loc url
-          xml.lastmod File.mtime(file).iso8601
+          xml.lastmod lastmod
         end
       end
     end
 
     File.write("site/sitemap.xml", xml.target!)
+
+    File.write(HASH_FILE, new_hashes.to_yaml)
   end
 end
