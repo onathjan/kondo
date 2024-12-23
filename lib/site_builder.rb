7e52b8a73613b2670ffb9a805dd1f69fb4dcd105
Jonathan Christian <christianj707@gmail.com>
2024-12-23 13:15:35 -0800

    add build_index_page and build_pages methods

---
+++ lib/site_builder.rb
@@ -0,0 +1,59 @@
+module SiteBuilder
+  require_relative 'content_processor'
+  require_relative 'template_renderer'
+  
+  def build_index_page
+    file_content = File.read("content/index.md")
+  
+    file_content.sub!(/^(---\s*\n.*?\n---)[\s\S]*/m, '\1')
+  
+    File.open("content/index.md", 'w') do |file|
+      file.puts file_content
+      file.puts ""
+    end
+  
+    posts = []
+  
+    Dir.glob("content/posts/*md").each do |file|
+      front_matter, _ = ContentProcessor.read_front_matter(file)
+      next if front_matter['draft'] == true
+      title = front_matter['title']
+      date = front_matter['date'].to_s
+      year = front_matter['date'].to_s[0..4]
+      slug = front_matter['slug'] + ".html" 
+      posts << { year: year, date: date, title: title, slug: slug }
+    end
+  
+    posts.sort_by! { |post| post[:date] }.reverse!
+  
+    posts_content = posts.map do |post|
+      <<~POST_LINK
+        ###### #{post[:date]}
+        ## [#{post[:title]}](#{post[:slug]})
+      POST_LINK
+    end.join("\n")
+  
+    open("content/index.md", 'a') { |f|
+      f.puts posts_content
+    }
+  end
+  
+  def build_pages(content_dir)
+    Dir.glob("content/**/*.md").each do |file|
+      front_matter, body = ContentProcessor.read_front_matter(file)
+      html_content = ContentProcessor.process_markdown(body)
+  
+      next if front_matter['draft']
+  
+      assigns = {
+        'title' => front_matter['title'],
+        'date' => front_matter['date'],
+        'content' => html_content
+      }
+  
+      rendered_content = TemplateRenderer.render_template("templates/layout.liquid", assigns)
+      output_file = "site/#{front_matter['slug']}.html"
+      File.write(output_file, rendered_content)
+    end
+  end
+end
