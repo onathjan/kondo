b6e5d0185abbeda6a68c1eab58e96ad04b4c10b4
Jonathan Christian <christianj707@gmail.com>
2024-12-23 13:40:16 -0800

    add self to method

--- lib/template_renderer.rb
+++ lib/template_renderer.rb
@@ -1,7 +1,7 @@
 module TemplateRenderer
   require 'liquid'
 
-  def render_template(template_file, assigns)
+  def self.render_template(template_file, assigns)
     template = File.read(template_file)
     Liquid::Template.parse(template).render(assigns)
   end
